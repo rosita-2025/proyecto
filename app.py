54ff713fc5d212c01c3c60465ec26c9fd181338e
rosita-2025 <rosamarescuela2020@gmail.com>
2025-06-02 11:05:17 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import torch
+from PIL import Image
+import io
+
+app = Flask(_name_)
+
+# Cargar modelo YOLOv5 desde archivo
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='impresion.pt', force_reload=True)
+
+@app.route("/inferencia", methods=["POST"])
+def inferencia():
+    if "file" not in request.files:
+        return jsonify({"error": "No se envió ninguna imagen"}), 400
+
+    image_file = request.files["file"]
+    img = Image.open(image_file.stream)
+
+    # Realiza inferencia
+    results = model(img)
+
+    # Convierte resultados a JSON
+    result_json = results.pandas().xyxy[0].to_json(orient="records")
+    return result_json
